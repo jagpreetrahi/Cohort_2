35df860528c50e3e12c0a0c53a15c2f4212cf74f
jagpreetrahi <jagpreet1615@gmail.com>
2025-01-30 22:25:26 +0530

    Week_4 assignment based on DOM manipulation

--- week4/calculator_backend.js
+++ week4/calculator_backend.js
@@ -1,19 +1,31 @@
 const express = require('express');
+const cors = require('cors');
 
-const PORT = 3010;
+const PORT = 3000;
 
 const app = express();
 
-
+app.use(cors())
 app.use(express.json());
 
 
 app.get('/sum' , function (req , res) {
     const a = req.query.a;
     const b = req.query.b;
-    console.log(b);
-    const sum = a + b;
-    return res.send(sum);
+    if(!isNaN(a) && !isNaN(b)){
+        const sum = parseInt(a) + parseInt(b);
+       return  res.json(sum)
+    }
+
+    else{
+        res.status(400).json({
+            message : "Invalid nUmber"
+        })
+    }
+
+   
+     
+    
 })
 
 
